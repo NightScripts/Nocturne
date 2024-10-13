bac75ec893d82ccf83fc3af7a6158c1a0a9bbc20
Night <166496112+NightScripts@users.noreply.github.com>
2024-10-13 22:07:26 +0800

    Update Auto Meditate.lua
    Open Source| Ninja Simulator | Auto Meditate | Nocturne.lua | 1.5A | Oct, 13, 2024 [9:53 PM]
    
    [10:06 PM] - Changes (atom changes lmao)

--- Ninja Simulator/Open Source/Auto Meditate.lua
+++ Ninja Simulator/Open Source/Auto Meditate.lua
@@ -3,7 +3,6 @@
 
 --// ©️ NOCTURNE
 local Nocturne = {}
-local ch = "true" --// does all the work, rel
 
 --// Services
 
@@ -20,39 +19,41 @@ local PlayerCharacter = Player.Character or Player.CharacterAdded:Wait()
 
 Nocturne.AutoMeditate = true --// set to false to NOT run
 
---// Main Func
-function AutoMeditate()
-  Nocturne.RemoteMed = PlayerCharacter:FindFirstChild("Meditate"):FindFirstChild("LocalScript")
-  ReplicatedStorage.Remotes.Metitate:InvokeServer(Nocturne.RemoteMed)
-end
-
 --// Handler/Checks
 
-local MeditateEquipped = false
-local Printed = false
+local isMeditateEquipped = false
+local hasPrintedMessage = false
 
-PlayerCharacter.ChildAdded:Connect(function(shit) --// this thing basically listens if the plr equipped the tool
-    if shit:IsA("Tool") and shit.Name == "Meditate" then
-      print("Check: True | EQUIPPED")
-      MeditatedEquipped = true
-      Printed = false
+--// Main Func
+function AutoMeditate()
+  Nocturne.Remote1 = PlayerCharacter:FindFirstChild("Meditate"):FindFirstChild("LocalScript")
+  ReplicatedStorage.Remotes.Metitate:InvokeServer(Nocturne.Remote1)
+end
+
+--// this thing basically listens if the plr equipped the tool
+PlayerCharacter.ChildAdded:Connect(function(nig)
+    if nig:IsA("Tool") and nig.Name == "Meditate" then 
+        print("Check: True")
+        isMeditateEquipped = true
+        hasPrintedMessage = false
     end
 end)
 
-PlayerCharacter.ChildRemoved:Connect(function(ass) --// this thing basically listens if the plr unequipped the tool
-    if ass:IsA("Tool") and ass.Name == "Meditate" then
-        MeditateEquipped = false
+--// this thing basically listens if the plr unequipped the tool
+PlayerCharacter.ChildRemoved:Connect(function(ger)
+    if ger:IsA("Tool") and ger.Name == "Meditate" then
+        isMeditateEquipped = false
     end
 end)
 
-RunService.RenderStepped:Connect(function() --// the end woooooooooooooowowowowowow
+RunService.RenderStepped:Connect(function() --// Main Handler bby
     if Nocturne.AutoMeditate then
-        if MeditateEquipped then
+        if isMeditateEquipped then
             AutoMeditate()
         else
-            if not Printed then
-                print(ch)
-                Printed = true
+            if not hasPrintedMessage then
+                print("shit")
+                hasPrintedMessage = true
             end
         end
     end
